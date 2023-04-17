0ff0189c2b16c74b8b5321038e930fd1a08f9fc4
gnarlyeah <118649910+gnarlyeah@users.noreply.github.com>
2023-04-17 21:41:23 +0800

    Create geolocation.js

---
+++ geolocation.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "store.php?lat=" + position.coords.latitude + "&long=" + position.coords.longitude);
+    xhttp.send();
+}
